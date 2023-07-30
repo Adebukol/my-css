5c777359904e98f5d4f1ffc685e7bc75c1a371c6
Adebukol <adebukolaomowole@gmail.com>
2023-07-30 23:25:50 +0100

    index.html updated with lightbox

--- server.js
+++ server.js
@@ -13,13 +13,20 @@
  function menutoggle(){
     navLinks.style.display = "block"
   }
-
+  const overlay =document.getElementById('overlay')
+  const closeOverlay = document.querySelector('.close-thumbail')
+  closeOverlay.onclick = function(){
+    overlay.style.display = 'block';
+  }
 
 // js for product details page----------------------------------
 const productImg = document.getElementById("product-img");
 const thumbnail= document.getElementsByClassName("product-thumbnail")
+const nextButton = document.getElementById('nextBtn');
+const prevButton = document.getElementById('previousBtn');
 
-thumbnail[0].onclick = function(){
+
+nextButton.onclick = function(){
   productImg.src = "images/image-product-1.jpg";
 }
 thumbnail[1].onclick = function(){
@@ -33,51 +40,51 @@ thumbnail[3].onclick = function(){
 }
 
 //-----------------cartjs
-const product = [
-  {
-    id:0,
-    image:"images/image-product-1.jpg",
-    title:"White and Carton Sneakers",
-    price:100,
-  },
-  {
-    id:1,
-    image:"images/black-shoe.png",
-    title:"Black Curtis shoe",
-    price:70,
-  },
-  {
-    id:2,
-    image:"images/bag4.png",
-    title:"Pink Handbag",
-    price:50,
-  },
-  {
-    id:3,
-    image:"images/blue-trainers.png",
-    title:"Blue trainers",
-    price:50,
-  }
-];
+// const product = [
+//   {
+//     id:0,
+//     image:"images/image-product-1.jpg",
+//     title:"White and Carton Sneakers",
+//     price:100,
+//   },
+//   {
+//     id:1,
+//     image:"images/black-shoe.png",
+//     title:"Black Curtis shoe",
+//     price:70,
+//   },
+//   {
+//     id:2,
+//     image:"images/bag4.png",
+//     title:"Pink Handbag",
+//     price:50,
+//   },
+//   {
+//     id:3,
+//     image:"images/blue-trainers.png",
+//     title:"Blue trainers",
+//     price:50,
+//   }
+// ];
 
-const categories = [...new Set(product.map((item)=>
-  {return item}))]
-  let i=0;
-  document.getElementById("productRoot").innerHTML = categories.map((item)=>
-  {
-    var {image, title, price} = item;
-    return(
-      `<div class="row">
-        <div class="col-1">
-          <img class="img-dit" src=${image}></img>
-          </div>
-          <div class="col-1">
-          <p>${title}<p>
-          <h4>$ ${price}.00</h4>`+
-          "<button onclick ='addtocart("+(i++)+")'>Add To Cart</button>" +
-          `</div>
-          </div>`
-    )
-  }).join('')
+// const categories = [...new Set(product.map((item)=>
+//   {return item}))]
+//   let i=0;
+//   document.getElementById("productRoot").innerHTML = categories.map((item)=>
+//   {
+//     var {image, title, price} = item;
+//     return(
+//       `<div class="row">
+//         <div class="col-1">
+//           <img class="img-dit" src=${image}></img>
+//           </div>
+//           <div class="col-1">
+//           <p>${title}<p>
+//           <h4>$ ${price}.00</h4>`+
+//           "<button onclick ='addtocart("+(i++)+")'>Add To Cart</button>" +
+//           `</div>
+//           </div>`
+//     )
+//   }).join('')
 
  
